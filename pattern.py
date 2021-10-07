22adccef4c4fdef9d9fdd7b298508c829abd465b
Ram Varra <ramvarra@hotmail.com>
2021-10-06 17:11:04 -0700

    Update pattern.py

--- pattern.py
+++ pattern.py
@@ -12,8 +12,11 @@ class ThreeDPoint(Point):
 @dataclass
 class CenterPoint(Point):
     z: int = 0
+@dataclass
+class Fruit:
+    name: str
         
-pts = [Point(10, 50), CenterPoint(10, 20, 30), ThreeDPoint(10, 20, 30)]
+pts = [Point(10, 50), CenterPoint(10, 20, 30), ThreeDPoint(10, 20, 30), Fruit("apple")]
 
 for pt in pts:
     match pt:
@@ -24,11 +27,12 @@ for pt in pts:
         case Point(x=10, y=_):
             print(f"2D This it- {pt}")
         case _:
-            print(f"Not it - {pt}")
+            print(f"Not a point: {pt}")
 """
 Should produce following print output:
 
 2D This it- Point(x=10, y=50)
 We are Center: CenterPoint(x=10, y=20, z=30)
 We are 3d: ThreeDPoint(x=10, y=20, z=30)
+Not a point: Fruit(name="apple")
 """
